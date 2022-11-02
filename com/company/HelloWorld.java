6cdc136239c1462221dfbfa77d7bc257d23efa44
Juan Pablo <juanpablocamus28@gmail.com>
2022-11-02 17:55:50 -0300

    first commit

---
+++ com/company/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class HelloWorld{
+  public static void main(String[] args) {
+    // Public indica que es publico, es decir que puede ser ejecutado por fuera de la clase
+    // Static que pertenece a la clase HelloWorld
+    // Void es el tipo de retorno, en este caso nada
+    // Main indica que es el metodo principal, punto de entrada de la APP. Normalmente hay uno solo siempre
+    // String[] args -> array de argumentos
+    System.out.println("Hello World"); //Snippet Sout
+  }
+}
---
+++ com/company/documentacion.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class documentacion {
+  public static void main(String[] args) {
+    
+  }
+}
