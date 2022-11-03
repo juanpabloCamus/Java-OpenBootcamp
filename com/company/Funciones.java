eb26c62ea824c87c67cf9c6993f8d06d40ef2b1e
Juan Pablo <juanpablocamus28@gmail.com>
2022-11-03 08:40:51 -0300

    funciones

---
+++ com/company/Funciones.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Funciones {
+  public static void main(String[] args) {
+    holaMundo("Roberto");
+  }
+
+  private static void holaMundo(String name) {
+    System.out.println("Hola" + name);
+  }
+}
